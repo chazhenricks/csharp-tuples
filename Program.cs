629d6b8cbc709afa4fd798d191d29ab08a98a711
Chaz Henricks <chazhenricks@gmail.com>
2017-07-07 14:18:23 -0500

    basic manipulations of lists and tuples

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>();
+           (string product, double amount, int quantity) Beer = ("Yngling", 9.99, 2);
+           (string product, double amount, int quantity) Cigarettes = ("Camel", 6.99, 1);
+           (string product, double amount, int quantity) Liquor = ("Jack Daniels", 15.99, 12);
+           (string product, double amount, int quantity) Cheese = ("Swiss", 1.99, 20);
+           (string product, double amount, int quantity) Snacks = ("Cheetos", 3.99, 25);
+
+           transactions.Add(Beer);
+           transactions.Add(Cigarettes);
+           transactions.Add(Liquor);
+           transactions.Add(Cheese);
+           transactions.Add(Snacks);
+
+           foreach ((string product, double amount, int quantity) t in transactions){
+               Console.WriteLine($"{t.product} {t.amount} {t.quantity}");
+           }
+
+        }
+    }
+}
